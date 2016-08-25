7da900055fd49fe79bd9388574e573f8e97a7cdf
Dmitriy Stradzhu <dimmua2013@gmail.com>
2016-08-25 19:09:38 +0300

    Выполнил 17-ое задание. Шаблонизируй это.

--- src/reviews.js
+++ src/reviews.js
@@ -1,6 +1,17 @@
 'use strict';
 
 var reviews = [];
+var reviewFilter = document.querySelector('.reviews-filter');
+var reviewTemplate = document.getElementById('review-template');
+var elementToClone;
+
+reviewFilter.classList.add('invisible');
+
+if ('content' in reviewTemplate) {
+  elementToClone = reviewTemplate.content.querySelector('.review');
+} else {
+  elementToClone = reviewTemplate.querySelector('.review');
+}
 
 function createCallback(url, name) {
   var script = document.createElement('script');
@@ -8,9 +19,46 @@ function createCallback(url, name) {
   document.body.appendChild(script);
 }
 
-window.showFeedback = function(data) {
+window.getFeedback = function(data) {
+  var reviewsList = document.querySelector('.reviews-list');
   reviews = data;
-  console.log(reviews);
+  reviews.forEach(function(review) {
+    showFeedback(review, reviewsList);
+  });
 };
 
-createCallback('/api/reviews?callback=', 'showFeedback');
+function showFeedback(review, container) {
+  var element = elementToClone.cloneNode(true);
+  var WIDTH_ONE_STAR = 40;
+  var IMAGE_LOAD_TIMEOUT = 10000;
+  var authorPictureTimeout;
+  var authorPicture = new Image(124, 124);
+
+  element.querySelector('.review-author').title = review.author.name;
+  element.querySelector('.review-author').alt = review.author.name;
+  element.querySelector('.review-rating').style.width = (review.rating * WIDTH_ONE_STAR) + 'px';
+
+  authorPicture.onload = function(event) {
+    clearTimeout(authorPictureTimeout);
+    element.querySelector('.review-author').src = event.target.src;
+  };
+
+  authorPicture.onerror = function() {
+    element.classList.add('review-load-failure');
+  };
+
+  authorPictureTimeout = setTimeout(function() {
+    authorPicture.src = '';
+    authorPicture.onerror();
+  }, IMAGE_LOAD_TIMEOUT);
+
+  authorPicture.src = review.author.picture;
+
+  element.querySelector('.review-text').textContent = review.description;
+  element.querySelector('.review-quiz').title = review.review_usefulness;
+  container.appendChild(element);
+  return element;
+}
+
+createCallback('/api/reviews?callback=', 'getFeedback');
+reviewFilter.classList.remove('invisible');
