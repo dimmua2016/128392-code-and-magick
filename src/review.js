ce860c247b540554b7a3361d13fad86de722957a
Dmitriy Stradzhu <dimmua2013@gmail.com>
2016-09-03 02:32:36 +0300

    Выполнил следующее задание в этой ветке, чтобы тебе было удобнее проверить

--- src/review.js
+++ src/review.js
@@ -11,24 +11,65 @@ define(function() {
     elementToClone = reviewTemplate.querySelector('.review');
   }
 
-  return function(review, container) {
-    var element = elementToClone.cloneNode(true);
+  function Review(review) {
     var WIDTH_ONE_STAR = 40;
+    var self = this;
+    this.data = review;
+    this.element = elementToClone.cloneNode(true);
+
+    this.element.querySelector('.review-author').title = this.data.author.name;
+    this.element.querySelector('.review-author').alt = this.data.author.name;
+    this.element.querySelector('.review-rating').style.width = (this.data.rating * WIDTH_ONE_STAR) + 'px';
+    this.element.querySelector('.review-text').textContent = this.data.description;
+
+    this.answerYes = this.element.querySelector('.review-quiz-answer-yes');
+    this.answerNo = this.element.querySelector('.review-quiz-answer-no');
+
+    this.usefulness = this.data.review_usefulness;
+    this.setUsefulness(this.usefulness);
+
+    this.answerYes.onclick = this.answerNo.onclick = function(evt) {
+      self.setAnswer(evt.target);
+    };
+
+    this.loadImg(this.data.author.picture);
+  }
+
+  Review.prototype.setAnswer = (function(target) {
+    var isYes = target === this.answerYes;
+    this.answerYes.classList.toggle('review-quiz-answer-active', isYes);
+    this.answerNo.classList.toggle('review-quiz-answer-active', !isYes);
+    this.remove();
+
+    if (isYes) {
+      this.usefulness++;
+    } else {
+      this.usefulness--;
+    }
+    this.setUsefulness(this.usefulness);
+  });
+
+  Review.prototype.setUsefulness = (function(i) {
+    this.element.querySelector('.review-quiz').childNodes[2].textContent = (i > 0 ? '(+' : '(') + i + ')';
+  });
+
+  Review.prototype.remove = (function() {
+    this.answerYes.onclick = this.answerNo.onclick = null;
+  });
+
+  Review.prototype.loadImg = (function(src) {
     var IMAGE_LOAD_TIMEOUT = 10000;
     var authorPictureTimeout;
+    var self = this;
     var authorPicture = new Image(124, 124);
 
-    element.querySelector('.review-author').title = review.author.name;
-    element.querySelector('.review-author').alt = review.author.name;
-    element.querySelector('.review-rating').style.width = (review.rating * WIDTH_ONE_STAR) + 'px';
-
-    authorPicture.onload = function(event) {
+    authorPicture.onload = function(evt) {
       clearTimeout(authorPictureTimeout);
-      element.querySelector('.review-author').src = event.target.src;
+      self.element.querySelector('.review-author').src = evt.target.src;
     };
 
     authorPicture.onerror = function() {
-      element.classList.add('review-load-failure');
+      self.element.classList.add('review-load-failure');
     };
 
     authorPictureTimeout = setTimeout(function() {
@@ -36,11 +77,9 @@ define(function() {
       authorPicture.onerror();
     }, IMAGE_LOAD_TIMEOUT);
 
-    authorPicture.src = review.author.picture;
+    authorPicture.src = src;
+  });
 
-    element.querySelector('.review-text').textContent = review.description;
-    element.querySelector('.review-quiz').title = review.review_usefulness;
-    container.appendChild(element);
-    return element;
-  };
+  return Review;
+
 });
