513765875b249eccfff00087cda17de06714aa80
Dmitriy Stradzhu <dimmua2013@gmail.com>
2016-09-07 20:21:32 +0300

    Выполнил 22-ое задание. Постраничный вывод и делегирование событий

--- bin/data/filter.js
+++ bin/data/filter.js
@@ -1,5 +1,40 @@
 'use strict';
 
 module.exports = function(list, filterID) {
+  switch (filterID) {
+    case 'reviews-recent':
+      var THREE_DAYS = 1000 * 60 * 60 * 24 * 3; // sec * min * hour * day * 3
+      var threeDayAgo = Date.now() - THREE_DAYS;
+      return list.filter(function(item) {
+          var created = Number(new Date(item.created));
+          return created >= threeDayAgo;
+        })
+        .sort(function(a, b) {
+          return b.created - a.created;
+        });
+
+    case 'reviews-good':
+      var MIN_MARK = 3;
+      return list.filter(function(item) {
+          return item.rating >= MIN_MARK;
+        })
+        .sort(function(a, b) {
+          return b.rating - a.rating;
+        });
+
+    case 'reviews-bad':
+      var MAX_MARK = 3;
+      return list.filter(function(item) {
+          return item.rating < MAX_MARK;
+        })
+        .sort(function(a, b) {
+          return a.rating - b.rating;
+        });
+
+    case 'reviews-popular':
+      return list.sort(function(a, b) {
+        return b.review_usefulness - a.review_usefulness;
+      });
+  }
   return list;
 };
--- src/load.js
+++ src/load.js
@@ -1,9 +1,15 @@
 'use strict';
 
 define(function() {
-  return function(url, name) {
-    var script = document.createElement('script');
-    script.src = url + name;
-    document.body.appendChild(script);
+  return function(url, params, callback) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onload = function(evt) {
+      var loadedData = JSON.parse(evt.target.response);
+      callback(loadedData);
+    };
+
+    xhr.open('GET', url + '?from=' + params.from + '&to=' + params.to + '&filter=' + params.filter);
+    xhr.send();
   };
 });
